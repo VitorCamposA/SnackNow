a066001dafb63af0e78f3fc8a7ff1660f0fa868b
Bllatzz <bl4tz@hotmail.com>
2024-05-04 15:29:19 -0300

    teste

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    VAI TOMAR NO SEU CU VG CALVO FDP <br>
+    VOCE FALOU QUE SE DESSE ERRADO IA SE MATAR , ENTAO SMT
+</body>
+</html>
